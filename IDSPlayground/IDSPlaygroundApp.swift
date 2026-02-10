e5dd1055ed4aa16d40a82ff942b7cf8f3f1a4ae3
woodyli7 <woody.liyuhui@gmail.com>
2026-02-10 08:53:50 -0500

    Initial Commit

---
+++ IDSPlayground/IDSPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDSPlaygroundApp.swift
+//  IDSPlayground
+//
+//  Created by Woody Li  on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct IDSPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
